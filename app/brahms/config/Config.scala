fd5e83e68a6fd490cf41b63fce678c13916d82bd
Chris Brahms <brahms5@gmail.com>
2014-04-06 14:39:49 -0400

    Ok now we need a username/password

--- app/brahms/config/Config.scala
+++ app/brahms/config/Config.scala
@@ -1,13 +1,14 @@
 package brahms.config
 
 import org.springframework.context.annotation.{ComponentScan, Bean, Configuration}
-import com.mongodb.{MongoURI, Mongo}
+import com.mongodb.{MongoCredential, MongoURI, Mongo}
 import org.springframework.data.mongodb.core.{MongoTemplate, MongoFactoryBean}
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories
 import play.api.Play
 import play.api.Play.current
 import javax.inject.Singleton
 import brahms.util.WithLogging
+import org.springframework.data.authentication.UserCredentials
 
 @EnableMongoRepositories
 @Configuration
@@ -17,9 +18,20 @@ class Config extends WithLogging{
   @Bean
   def mongoTemplate() : MongoTemplate = {
     val uri = Play.configuration.getString("mongo.uri").get
-    val mongo = new Mongo(new MongoURI(uri))
+    val mongoUri = new MongoURI(uri)
+    val mongo = new Mongo()
+
     logger.debug("Got a mongo: " + mongo)
-    return new MongoTemplate(mongo, "stratego")
+    Play.configuration.getString("mongo.pass") match {
+      case Some(pass: String) =>
+        val credentials = new UserCredentials(
+          Play.configuration.getString("mongo.user").get,
+          pass
+        )
+        return new MongoTemplate(mongo, "stratego", credentials)
+      case _ =>
+        return new MongoTemplate(mongo, "stratego")
+    }
   }
 
 }
